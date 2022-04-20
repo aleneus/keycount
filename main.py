60b7c248ba591689e2c964eaa7a9ef7408ef1f96
Aleksandr Popov <aleneus@gmail.com>
2022-04-20 09:32:41 +0300

    feat: count different keys

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ class Counter:
     """Counter."""
 
     def __init__(self):
-        self.__counter = 0
+        self.__total = 0
+        self.__symbols = 0
+        self.__navi = 0
         self.__clear_command = 'clear'
 
     def on_key_press(self, event):
@@ -15,7 +17,14 @@ class Counter:
         if 'mouse' in str(event):
             return
 
-        self.__counter += 1
+        self.__total += 1
+
+        if is_symbol(event_get_key(event)):
+            self.__symbols += 1
+
+        if is_navi(event_get_key(event)):
+            self.__navi += 1
+
         self.__clear_output()
         self.__print_value()
 
@@ -23,7 +32,69 @@ class Counter:
         os.system(self.__clear_command)
 
     def __print_value(self):
-        print(f"keys total: {self.__counter}")
+        other = self.__total - self.__symbols - self.__navi
+
+        print(f"  total: {self.__total}")
+        print(f"symbols: {self.__symbols}")
+        print(f"   navi: {self.__navi}")
+        print(f"  other: {other}")
+
+
+def is_navi(key):
+    return key in [
+        'Up', 'Down', 'Left', 'Right', 'Home', 'End', 'Page_Up', 'Page_Down'
+    ]
+
+
+def is_symbol(key):
+    if key.upper() in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        return True
+
+    if key in "`1234567890":
+        return True
+
+    if key in [
+            'grave',
+            'asciitilde',
+            'exclam',
+            'at',
+            'numbersign',
+            'dollar',
+            'percent',
+            'asciicircum',
+            'ampersand',
+            'asterisk',
+            'parenleft',
+            'parenright',
+            'minus',
+            'underscore',
+            'equal',
+            'plus',
+            'comma',
+            'less',
+            'period',
+            'greater',
+            'slash',
+            'question',
+            'backslash',
+            'bar',
+            'bracketleft',
+            'braceleft',
+            'bracketright',
+            'braceright',
+            'apostrophe',
+            'quotedbl',
+    ]:
+        return True
+
+    if key in ["Return", "space"]:
+        return True
+
+    return False
+
+
+def event_get_key(event):
+    return event.Key
 
 
 def main():
